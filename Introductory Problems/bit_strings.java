e00763bb706ca47a4ef717acef14e13d9b87fe03
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2020-09-27 02:04:06 +0530

    Create bit_strings.java

---
+++ Introductory Problems/bit_strings.java
@@ -0,0 +1,111 @@
+// https://cses.fi/problemset/task/1617
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Cses
+{  
+ 
+    static int MOD = (int)(1e9 + 7);
+    static long exp(long a,long b)
+    {
+      long ans=1l;
+      a%=MOD;
+      if(b==0)
+        return 1l;
+      else if(b==1)
+        return a;
+      else
+      {
+        while(b>0)
+        {
+        if((b&1)==1)//odd
+        ans=(ans*a)%MOD;
+        b=b>>=1;
+        a=(a*a)%MOD;
+        }
+      }
+      return ans;
+    }
+ 
+    public static void process()throws IOException
+    {
+     long n=nl();
+     //permutation on filling n places with any 2 numbers i.e 0 and 1... ans=2^n;
+     pn(exp(2,n));
+ 
+    }
+    static AnotherReader sc;
+    static PrintWriter out;
+    public static void main(String[]args)throws IOException
+    {
+        out = new PrintWriter(System.out);
+        sc=new AnotherReader();
+        boolean oj = true;
+ 
+        //oj = System.getProperty("ONLINE_JUDGE") != null;
+        //if(!oj) sc=new AnotherReader(100);
+ 
+        long s = System.currentTimeMillis();
+      //int T=ni(); while(T-->0)
+            process();
+        out.flush();
+        if(!oj)
+            System.out.println(System.currentTimeMillis()-s+"ms");
+        System.out.close();  
+    }
+    static void sort(long arr[],int n)
+    {
+    shuffle(arr,n);
+    Arrays.sort(arr);
+    }
+    static void shuffle(long arr[],int n)
+    {
+        Random r=new Random();
+        for(int i=0;i<n;i++)
+        {
+          long temp=arr[i];
+          int pos=i+r.nextInt(n-i);
+          arr[i]=arr[pos];
+          arr[pos]=temp;
+        }
+    }
+ 
+    static void pn(Object o){out.println(o);}
+    static void p(Object o){out.print(o);}
+    static void pni(Object o){out.println(o);System.out.flush();}
+    static int ni()throws IOException{return sc.nextInt();}
+    static long nl()throws IOException{return sc.nextLong();}
+    static double nd()throws IOException{return sc.nextDouble();}
+    static String nln()throws IOException{return sc.nextLine();}
+    static String nn()throws IOException{return sc.next();}
+    static long gcd(long a, long b)throws IOException{return (b==0)?a:gcd(b,a%b);}
+    static int gcd(int a, int b)throws IOException{return (b==0)?a:gcd(b,a%b);}
+    static int bit(long n)throws IOException{return (n==0)?0:(1+bit(n&(n-1)));}
+    static int[] iarr(int N)throws IOException{int[]ARR=new int[N];for(int i=0;i<N;i++){ARR[i]=ni();}return ARR;}
+    static long[] larr(int N)throws IOException{long[]ARR=new long[N];for(int i=0;i<N;i++){ARR[i]=nl();}return ARR;}
+    static boolean multipleTC=false;
+ 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static class AnotherReader{BufferedReader br; StringTokenizer st;
+    AnotherReader()throws FileNotFoundException{
+    br=new BufferedReader(new InputStreamReader(System.in));}
+    AnotherReader(int a)throws FileNotFoundException{
+    br = new BufferedReader(new FileReader("input.txt"));}
+    String next()throws IOException{
+    while (st == null || !st.hasMoreElements()) {try{
+    st = new StringTokenizer(br.readLine());}
+    catch (IOException  e){ e.printStackTrace(); }}
+    return st.nextToken(); } int nextInt() throws IOException{
+    return Integer.parseInt(next());}
+    long nextLong() throws IOException
+    {return Long.parseLong(next());}
+    double nextDouble()throws IOException { return Double.parseDouble(next()); }
+    String nextLine() throws IOException{ String str = ""; try{
+    str = br.readLine();} catch (IOException e){
+    e.printStackTrace();} return str;}}
+    
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+    
+  
